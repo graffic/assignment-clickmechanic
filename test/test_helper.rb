c67cf3f56b634b8c513aaa2129624245e26e1dd6
Javier Gonel <bolibic@gmail.com>
2017-06-05 09:39:46 +0300

    Enable coveralls.io

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 require 'simplecov'
+require 'coveralls'
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start 'test_frameworks'
 
 require "minitest/autorun"
